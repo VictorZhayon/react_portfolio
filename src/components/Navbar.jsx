5d96c7993970a44bc292c4314943b10472f44bac
VictorZhayon <victorzion1@gmail.com>
2025-01-09 10:48:27 +0100

    works on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,14 +8,25 @@ import { nav } from 'framer-motion/client';
 
 
 const Navbar = () => {
+  const [active, setActive] = useState("")
   return (
     <nav
       className= {
-      '${styles.paddindX}'
+        '${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary'
       }
     >
-
-
+      <div className='w-full flex justify-between items-center max-w-7x1 mx-auto'>
+        <Link 
+          to ="/"
+          className='flex items-center gap2'
+          onClick={() =>{
+            setActive("");
+            window.scrollTo(0,0);
+          }}
+        >
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+        </Link>
+      </div>
     </nav>
   )
 }
